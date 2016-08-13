0853675c45a031b96449cf85dba707de2fc4febd
Canna Wen <cannawen@gmail.com>
2016-08-13 11:36:05 -0400

    Refactor to reduce response-handling duplication

--- 9-async.js
+++ 9-async.js
@@ -5,23 +5,19 @@ const url1 = process.argv[2];
 const url2 = process.argv[3];
 const url3 = process.argv[4];
 
-http.get(url1, function (response) {
+http.get(url1, function (response1) {
+	handleResponse(response1);
+	http.get(url2, function (response2) {
+		handleResponse(response2)
+		http.get(url3, function (response3) {
+			handleResponse(response3)
+		})
+	})
+})
+
+function handleResponse(response) {
 	response.pipe(bl(function (error, data) {
 		const stringData = data.toString();
 		console.log(stringData);
-
-		http.get(url2, function (response) {
-			response.pipe(bl(function (error, data) {
-				const stringData = data.toString();
-				console.log(stringData);
-
-				http.get(url3, function (response) {
-					response.pipe(bl(function (error, data) {
-						const stringData = data.toString();
-						console.log(stringData);
-					}))
-				})
-			}))
-		})
 	}))
-})
+}
